ef8c9a2fa88d03a4db822c93c038e33b322642e1
louiswust@fastmail.fm <louiswust@fastmail.fm>
2017-03-21 21:16:08 -0400

    Start Over button now warns before taking effect

--- freemoney/static/freemoney/freemoney.js
+++ freemoney/static/freemoney/freemoney.js
@@ -9,3 +9,11 @@ function register_close_warning() {
     $(window).off('beforeunload.freemoney');
   });
 }
+$(function () {
+  var b = $('button[name="submit-type"][value="restart"][id!="restart-for-real"]');
+  b.on('click', function (e) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    $('#dialog-restart-warn').modal('show');
+  });
+});
